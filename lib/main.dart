c6f6f6140ab443a365781306cdceb2c9a3e3c2e2
Asus <pavel@easital.com>
2024-04-21 07:50:38 +0600

    initialize projects

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 150,
-          color: Colors.lightGreen,
-          child: const Center(
-              child: Text(
-            "Hellow its SK raz here",
-            style: TextStyle(fontSize: 14),
-          )),
+        body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          colors: [Colors.orange, Colors.orangeAccent, Colors.deepOrange],
         ),
       ),
-    );
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 80,),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Login",style: TextStyle(color: Colors.white, fontSize: 40),),
+                    SizedBox(height: 10,),
+                    Text("Welcome Back",style: TextStyle(color: Colors.white, fontSize: 18),),
+                  ],
+                ),
+              )
+            ],
+          ),
+    ));
   }
 }
